67cf66ef6029506cebccb306a5da30d72e420523
Marc <marclucchini@gmail.com>
2019-09-05 15:07:00 +0100

    Replaced react-tabs with react-router-tabs and a router to enable tab history

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { NavTab } from "react-router-tabs";
 import Geolocation from './Geolocation';
 import Scanner from './Scanner';
 import AugmentedReality from './AugmentedReality';
 import Payment from './Payment';
 import InstallationPrompt from './InstallationPrompt';
 import LocalNotifications from './LocalNotifications';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './App.css';
-import 'react-tabs/style/react-tabs.css';
+import 'react-router-tabs/styles/react-router-tabs.css';
 
-function App() {
+const history = createBrowserHistory();
+
+const App = () => {
     return (
       <div className="App">
-          <Tabs>
-              <TabList>
-                  <Tab>Geolocation</Tab>
-                  <Tab>Scanner</Tab>
-                  <Tab>AR</Tab>
-                  <Tab>Payment</Tab>
-                  <Tab>Push</Tab>
-              </TabList>
-              <TabPanel>
-                  <Geolocation/>
-              </TabPanel>
-              <TabPanel>
-                  <Scanner/>
-              </TabPanel>
-              <TabPanel>
-                  <AugmentedReality/>
-              </TabPanel>
-              <TabPanel>
-                  <Payment/>
-              </TabPanel>
-              <TabPanel>
-                  <LocalNotifications/>
-              </TabPanel>
-          </Tabs>
+          <Router history={history}>
+              <NavTab to="/geolocation" replace={false}>Geolocation</NavTab>
+              <NavTab to="/scanner" replace={false}>Scanner</NavTab>
+              <NavTab to="/ar" replace={false}>AR</NavTab>
+              <NavTab to="/payment" replace={false}>Payment</NavTab>
+              <NavTab to="/notifications" replace={false}>Notifications</NavTab>
+              <Switch>
+                  <Route path={'/geolocation'} component={Geolocation} />
+                  <Route path={'/scanner'} component={Scanner} />
+                  <Route path={'/ar'} component={AugmentedReality} />
+                  <Route path={'/payment'} component={Payment} />
+                  <Route path={'/notifications'} component={LocalNotifications} />
+              </Switch>
+          </Router>
           <InstallationPrompt/>
       </div>
     );
